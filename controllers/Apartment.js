74efc2d5591ce70c4b89d46ddbe28f12f86abc59
VBSKishore <142835988+VBSKishore@users.noreply.github.com>
2023-11-09 20:43:19 -0600

    Staged

--- controllers/Apartment.js
+++ controllers/Apartment.js
@@ -65,7 +65,7 @@ res.status(500);
 res.send(`{"error": ${err}}`);
 }
 };
-// for a specific Costume.
+// for a specific Apartment.
 exports.Apartment_detail = async function(req, res) {
 console.log("detail" + req.params.id)
 try {
@@ -74,6 +74,27 @@ res.send(result)
 } catch (error) {
 res.status(500)
 res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+
+//Handle Apartment update form on PUT.
+exports.Apartment_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Apartment.findById( req.params.id)
+// Do updates of properties
+if(req.body.name)
+toUpdate.name = req.body.name;
+if(req.body.price) toUpdate.price = req.body.price;
+if(req.body.Location) toUpdate.Location = req.body.Location;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
 }
 };
 
