a96e5ba0664c67d5d54944e5b56002c566268803
VBSKishore <142835988+VBSKishore@users.noreply.github.com>
2023-11-28 22:37:17 -0600

    added apartments

--- routes/Apartment.js
+++ routes/Apartment.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const Apartment_controlers= require('../controllers/Apartment');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    res.redirect("/login");
+  } 
 /* GET Apartments */
 router.get('/', Apartment_controlers.Apartment_view_all_Page );
 
@@ -10,8 +19,9 @@ router.get('/detail', Apartment_controlers.Apartment_view_one_Page);
 /* GET create Apartment page */
 router.get('/create', Apartment_controlers.Apartment_create_Page);
 
-/* GET create update page */
-router.get('/update', Apartment_controlers.Apartment_update_Page);
+/* GET update costume page */
+router.get('/update', secured, Apartment_controlers.Apartment_update_Page);
+
 
 /* GET delete Apartment page */
 router.get('/delete', Apartment_controlers.Apartment_delete_Page);
