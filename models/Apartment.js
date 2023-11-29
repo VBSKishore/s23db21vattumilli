a3a7001570a7e6a95bd35b5c1bed502dcd4d505e
VBSKishore <142835988+VBSKishore@users.noreply.github.com>
2023-11-29 00:07:22 -0600

    addded validation code

--- models/Apartment.js
+++ models/Apartment.js
@@ -2,20 +2,20 @@ const mongoose = require("mongoose")
 const ApartmentSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true
-
-    },
-    Location: {
-        type: String,
-        required: true
-
+        required: true,
     },
     price: {
         type: Number,
         required: true,
         min: [1, 'Quantity must be at least 1'],  // Minimum value and error message
-        max: [1000000, 'Quantity cannot exceed 1000000'],
-    }
+        max: [1000000, 'Quantity cannot exceed 100'],
 
+
+    },
+
+    Location: {
+        type: String,
+        required: true,
+    }
 });
 module.exports = mongoose.model("Apartment", ApartmentSchema)
